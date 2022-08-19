46d7f49c624d77b4c1570f51af8cd34ad2eb2cdd
Icaom <110424325+Icaom@users.noreply.github.com>
2022-08-19 16:43:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProjetoJavascript
+ Aula javascript
---
+++ index.js
@@ -0,0 +1,11 @@
+var socrates = 36
+var platao = -11
+var hercules = 250
+var jornal
+
+
+function Garen(jornal) { 
+    return jornal + (socrates + platao + hercules) 
+}
+
+console.log(Garen(0))
